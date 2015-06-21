9a89e563aa779de4d82c7049b9bd77339cd0958a
Niel <hu.niel92@gmail.com>
2015-06-21 10:31:46 -0400

    first version

--- Percolation.java
+++ Percolation.java
@@ -1,8 +1,8 @@
 public class Percolation
 {
 	
-	private boolean[] grid;	
-	private int N; 
+	private static boolean[] grid;	
+	private static int N; 
 	private UF uf; 	
 
 	public Percolation(int N)// create N-by-N grid, with all sites blocked
@@ -37,69 +37,98 @@ public class Percolation
 		
 		if( i == 1 && j == 1)
 		{
-			uf.union(N * (i - 1) + j , N * (i) + j);//down 
-			uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i) + j ] == true ) uf.union(N * (i - 1) + j , N * (i) + j);//down 
+			if( grid[ N * (i - 1) + j + 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
 		}else if( i == 1 && j == N)
 		{
-			uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
-			uf.union(N * (i - 1) + j , N * (i) + j);//down 
+			if( grid[ N * (i - 1) + j - 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
+			if( grid[ N * (i) + j ] == true ) uf.union(N * (i - 1) + j , N * (i) + j);//down 
 		}else if( i == N && j == 1)
 		{
-			uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
-			uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i - 2) + j ] == true ) uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
+			if( grid[ N * (i - 1) + j + 1 ] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
 		}else if( i == N && j == N)
 		{
-			uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
-			uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
+			if( grid[ N * (i - 2) + j ] == true ) uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
+			if( grid[ N * (i - 1) + j - 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
 		}else if( i == 1  )
 		{
-			uf.union(N * (i - 1) + j , N * (i) + j);//down 
-			uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
-			uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i) + j ] == true ) uf.union(N * (i - 1) + j , N * (i) + j);//down 
+			if( grid[ N * (i - 1) + j - 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
+			if( grid[ N * (i - 1) + j + 1 ] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
 		}
 		else  if ( i == N )
 		{
-			uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
-			uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
-			uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i - 2) + j ] == true ) uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
+			if( grid[ N * (i - 1) + j - 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
+			if( grid[ N * (i - 1) + j + 1 ] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
 		}else if ( j == 1)
 		{
-			uf.union(N * (i - 1) + j , N * (i) + j);//down 
-			uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
-			uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i) + j ] == true )  uf.union(N * (i - 1) + j , N * (i) + j);//down 
+			 if( grid[ N * (i - 2) + j ] == true ) uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
+			if( grid[ N * (i - 1) + j + 1 ] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
 		}else if( j == N)
 		{
-			uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
-			uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
-			uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i - 2) + j ] == true ) uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
+			if( grid[ N * (i - 1) + j - 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
+			if( grid[ N * (i - 1) + j + 1 ] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
 		}else 
-		{ StdOut.print(" connecting error"); StdOut.println();}
+		{ 
+			if( grid[ N * (i - 2) + j ] == true ) uf.union(N * (i - 1) + j , N * (i - 2) + j);//up
+			if( grid[ N * (i - 1) + j - 1] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j - 1);//left
+			if( grid[ N * (i - 1) + j + 1 ] == true ) uf.union(N * (i - 1) + j , N * (i - 1) + j + 1);//right
+			if( grid[ N * (i) + j ] == true )  uf.union(N * (i - 1) + j , N * (i) + j);//down 
+		}
 	}
 	
-	public boolean isOpen(int i, int j)
+	public static boolean isOpen(int i, int j)
 	{
 		return grid[N * (i - 1) + j];
 		
 	}
 	
-	
-
-	public void percolates()//does the system percolate?
+	public void grid_display()
 	{
-		StdOut.print(uf.connected(0,N*N + 1) + " ");
-		StdOut.println();
+		for(int i = 1; i <= N; i++)
+		{
+			for(int j = 1; j <= N; j++)
+			{
+				StdOut.print( grid[N * (i - 1) + j] + " " );
+			}
+			StdOut.println();
+		}
+	}
+			
+
+	public boolean percolates()//does the system percolate?
+	{
+		//StdOut.print(uf.connected(0,N*N + 1) + " ");
+		//StdOut.println();
+		return 	uf.connected(0,N*N + 1);
 	}
 	
-	public static void main(String[] args)
-	{
-		Percolation pc = new Percolation(4);
-		pc.open(1,1);
-		pc.open(2,1);
-		pc.open(3,1);
-		pc.percolates();
-		StdOut.print(pc.isOpen(3,1));
-
-	}		
 	
 
+
+	public static void main(String[] args)
+	{
+		/*int count = 0;		
+		Percolation pc = new Percolation(200);
+		while( !pc.percolates())
+		{
+			int row = StdRandom.uniform(1,N+1);
+			int column = StdRandom.uniform(1,N+1);
+			if( isOpen(row,column) ) continue;
+			else
+			{
+				StdOut.print( "row :" + row +  "column :" + column + " ");	
+				StdOut.println();		
+				pc.open(row,column);
+				count++;
+			}		
+		}		
+		pc.grid_display();
+		StdOut.println();
+		StdOut.print(count + " ");*/
+	}		
 }	
